a6cd21f090a8da310db2358d2d043e473114e3a4
Yacine Belal <yacine.belal@cea.fr>
2026-06-09 16:11:56 +0200

    Add model building through registry and random env init in main train pipeline

--- baselines/mitbih_train_pipeline.py
+++ baselines/mitbih_train_pipeline.py
@@ -8,38 +8,33 @@ from torch.utils.data import DataLoader
 
 from baselines.data import MitbihDataset, load_mit_bih
 from baselines.models import (
-    CNN,
+    build_model,
     test_model,
     train_model,
 )
+from baselines.utils import init_randomized_envs
 
 
 def main(
-        window_len = 128,
-        epochs = 30,
-        batch_size = 32,
-        normal_class= 0, #TODO: do not provide as argument, collect instead
-        lr = 0.0001,
-        mode = "design",
-        #only in mode=design
-        val_size=0.1, 
-        val_period=2,
-        seed = 42,
+    window_len=128,
+    model_name="CNN",
+    epochs=30,
+    batch_size=128,
+    normal_class=0,  # TODO: do not provide as argument, collect instead
+    lr=0.001,
+    mode="final",
+    val_size=0.1,
+    val_period=5,
+    log_to_mlflow=False,
+    seed=42,
 ):
-    torch.manual_seed(seed)
-    np.random.seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
-
+    init_randomized_envs(seed)
     DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    dataset = load_mit_bih(val_size=val_size)
+    dataset = load_mit_bih(val_size=val_size, mode=mode)
 
-    train_dl = DataLoader(
-        MitbihDataset(*dataset["train"]),
-        batch_size=batch_size,
-        shuffle=True)
-    
+    train_dl = DataLoader(MitbihDataset(*dataset["train"]), batch_size=batch_size, shuffle=True)
+
     test_dl = DataLoader(
         MitbihDataset(*dataset["test"]),
         batch_size=1024,
@@ -48,10 +43,13 @@ def main(
 
     mlflow.set_experiment("CNN MIT-BIH")
     with mlflow.start_run():
-        model = CNN().to(DEVICE)
+        model = build_model(model_name).to(DEVICE)
         optimizer = torch.optim.Adam(model.parameters(), lr=lr)
         classes = np.unique(dataset["train"][-1])
+        print(classes)
+        print(dataset["classes_names"])
         weights = compute_class_weight("balanced", classes=classes, y=dataset["train"][-1])
+
         weights = torch.Tensor(weights).to(DEVICE)
         criterion = nn.CrossEntropyLoss(weight=weights, reduction="sum")
         mlflow.log_params(
@@ -77,12 +75,21 @@ def main(
         else:
             train_model(model, train_dl, optimizer, criterion, epochs, device=DEVICE)
 
-        results = test_model(model, test_dl, criterion, normal_class=normal_class, device=DEVICE, log_artifacts=True)
+        results = test_model(
+            model,
+            test_dl,
+            criterion,
+            normal_class=normal_class,
+            class_names=dataset["classes_names"],
+            device=DEVICE,
+            log_artifacts=True,
+        )
         test_metrics = {f"test/{k}": v for k, v in results[0].items()}
         print(test_metrics)
-        mlflow.log_metrics(test_metrics)
-        mlflow.log_text(str(model), "architecture.txt")
-        mlflow.set_tags({"model_type": "CNN", "dataset": "MIT-BIH"})
+        if log_to_mlflow:
+            mlflow.log_metrics(test_metrics)
+            mlflow.log_text(str(model), "architecture.txt")
+            mlflow.set_tags({"model_type": model_name, "dataset": "MIT-BIH"})
 
 
 if __name__ == "__main__":
